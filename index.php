e4eaa9749404f94f9618f8684517571d9c6eb69e
Felix Nugroho <lyxthor22@gmail.com>
2023-08-11 14:42:00 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Git Remote Test 2</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas iusto, amet quod ullam, ipsam molestiae earum id et sed beatae cupiditate velit eaque minus modi eum, odio dolorem numquam vitae nobis? Odio aperiam id libero similique, minus, officiis atque impedit repellendus suscipit iusto voluptatum vel. Veritatis eum voluptatem nobis inventore.</p>
+</body>
+</html>
