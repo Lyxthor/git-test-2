29b6e703c1453024f41b6cd68fa184beba237b21
Felix Nugroho <lyxthor22@gmail.com>
2023-08-11 15:11:37 +0700

    menambahkan list di file index.php

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas iusto, amet quod ullam, ipsam molestiae earum id et sed beatae cupiditate velit eaque minus modi eum, odio dolorem numquam vitae nobis? Odio aperiam id libero similique, minus, officiis atque impedit repellendus suscipit iusto voluptatum vel. Veritatis eum voluptatem nobis inventore.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
